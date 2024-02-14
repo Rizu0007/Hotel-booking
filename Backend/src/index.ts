7640d9d5fa4a6e19106a91400ecf9650afa81563
Rizwan ali <87184120+Rizu0007@users.noreply.github.com>
2024-02-14 12:42:25 +0500

    Setup

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -6,6 +6,7 @@ import userRoutes from './routes/users'
 import authRoutes from './routes/auth'
 
 import cookieParser from "cookie-parser";
+import path from 'path';
 mongoose.connect(process.env.MONGODB_CONNECTION as string)
 
 const app =express(); 
@@ -16,6 +17,7 @@ app.use(cors({
  origin:process.env.FRONTEND_URL,
     credentials:true,
 }));
+app.use(express.static(path.join(__dirname, "../../frontend/dist")));
 
 app.use("/api/auth" ,authRoutes);
 app.use("/api/users" , userRoutes);
